c666159d6f1feca456a08495c7fefea45b1d8e92
AstroGit <masaladosa6@gmail.com>
2021-12-05 14:41:12 +0300

    83 reselect

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ import {Provider} from 'react-redux';
     document.getElementById('root')
   );
 
+
+//ServiceWorker.unregister();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
