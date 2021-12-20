7d55e7bff66f19dfb58fa3d08eb217a4741b8e49
AstroGit <masaladosa6@gmail.com>
2021-12-20 13:29:11 +0300

    96 lesson Profile image download

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -4,6 +4,7 @@ const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
 const DELETE_POST = 'DELETE_POST';
+const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
 let initialState = {
         posts: [
@@ -37,14 +38,21 @@ const profileReducer = (state = initialState, action) => {
                 status: action.status
             };
         }
+
         case SET_USER_PROFILE: {
             return {
                 ...state, profile: action.profile
             }
         }
+
         case DELETE_POST: {
             return {...state, posts: state.posts.filter(p => p.id != action.postId)}
         }
+
+        case SAVE_PHOTO_SUCCESS: {
+            return {...state, profile: {...state.profile, photos: action.photos} }
+        }
+
         default: return state;
     }
 }
@@ -53,19 +61,30 @@ export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostTe
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
 export const deletePost = (postId) => ({type: DELETE_POST, postId});
+export const savePhotoSuccess = (photos) => ({type: SAVE_PHOTO_SUCCESS, photos});
 
 export const getUserProfile = (userId) => async (dispatch) => {
     const response = await usersAPI.getProfile(userId);
         dispatch(setUserProfile(response.data));
 }
+
 export const getStatus = (userId) => async (dispatch) => {
     const response = await profileAPI.getStatus(userId); 
         dispatch(setStatus(response.data));
 }
+
 export const updateStatus = (status) => async (dispatch) => {
     const response = await profileAPI.updateStatus(status);
         if (response.data.resultCode === 0) {
         dispatch(setStatus(status));
+        }
+}
+
+export const savePhoto = (file) => async (dispatch) => {
+    const response = await profileAPI.savePhoto(file);
+
+        if (response.data.resultCode === 0) {
+        dispatch(savePhotoSuccess(response.data.data.photos));
         }
 }
 
